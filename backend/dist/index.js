e14dda9b6096fc6a75fd7872713ebaed1f907786
Tonnel <154825284+T0nnel@users.noreply.github.com>
2024-07-26 13:15:56 +0300

    ssssss

---
+++ backend/dist/index.js
@@ -0,0 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
+const productRoutes_1 = __importDefault(require("./routes/productRoutes"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const PORT = 5000;
+app.use(express_1.default.json());
+mongoose_1.default.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+app.use('/api/users', userRoutes_1.default);
+app.use('/api/products', productRoutes_1.default);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+//# sourceMappingURL=index.js.map
